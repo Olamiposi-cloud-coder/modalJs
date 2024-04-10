8ac1f02c0f5067e9dd8926540426358beebaa3b5
Olamiposi <olamiposicloud25@gmail.com>
2024-04-10 22:55:52 +0100

    few lines of code written

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalBtn = document.querySelectorAll('.open-modal');
 
-for (let i = 0; i < openModalBtn.length; i++) console.log();
-openModalBtn[i].addEventListener('click', function () {});
+for (let i = 0; i < openModalBtn.length; i++)
+  openModalBtn[i].addEventListener('click', function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+closeModalBtn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
