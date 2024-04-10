3ff5b5a727368bbe123627b9d0be9480fc662c99
Olamiposi <olamiposicloud25@gmail.com>
2024-04-10 22:37:34 +0100

    working on the js

--- script.js
+++ script.js
@@ -5,7 +5,5 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalBtn = document.querySelectorAll('.open-modal');
 
-console.log(openModalBtn);
-
-for (let i = 0; i < openModalBtn.length; i++)
-  console.log(openModalBtn[i].textContent);
+for (let i = 0; i < openModalBtn.length; i++) console.log();
+openModalBtn[i].addEventListener('click', function () {});
