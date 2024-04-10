88ea39699374b79d0f8942f65026e78d00e072ed
Olamiposi <olamiposicloud25@gmail.com>
2024-04-10 23:25:06 +0100

    sticking with DRY principle

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalBtn = document.querySelectorAll('.open-modal');
 
-for (let i = 0; i < openModalBtn.length; i++)
-  openModalBtn[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const openAndCloseModal = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
 
-closeModalBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+for (let i = 0; i < openModalBtn.length; i++) {
+  openModalBtn[i].addEventListener('click', openAndCloseModal);
+}
+
+closeModalBtn.addEventListener('click', openAndCloseModal);
