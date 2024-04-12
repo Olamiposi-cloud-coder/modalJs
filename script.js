9a2099caa8e9c6b059917c24d41fb943fef9663d
Olamiposi <olamiposicloud25@gmail.com>
2024-04-12 10:59:39 +0100

    fixing bugs

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const openModalBtn = document.querySelectorAll('.open-modal');
 
-const openAndCloseModal = function () {
-  modal.classList.toggle('hidden');
+const openAndCloseModal = function (event) {
+  const buttonClicked = event.target;
+  const modalId = buttonClicked.dataset.modalId;
+
+  const modalToOpen = document.getElementById('modalId');
+  modalToOpen.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
 };
 
-for (let i = 0; i < openModalBtn.length; i++) {
-  openModalBtn[i].addEventListener('click', openAndCloseModal);
-}
+openModalBtn.forEach(function (btn) {
+  btn.addEventListener('click', openAndCloseModal);
+});
 
-closeModalBtn.addEventListener('click', openAndCloseModal);
+if (closeModalBtn) {
+  closeModalBtn.addEventListener('click', function () {
+    const openModals = document.querySelectorAll('.modal:not(.hidden)');
+    openModals.forEach(modal => modal.classList.add('hidden'));
+  });
+}
