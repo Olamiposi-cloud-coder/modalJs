66decc2db650e6a6c46792b2cf9a7b89c09601d1
Olamiposi <olamiposicloud25@gmail.com>
2024-04-12 19:03:10 +0100

    Js code completed

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 'use strict';
 
-const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const closeModalBtn = document.querySelector('.close-modal');
+const closeModalBtn = document.querySelectorAll('.close-modal');
 const openModalBtn = document.querySelectorAll('.open-modal');
 
-const openAndCloseModal = function (event) {
-  const buttonClicked = event.target;
-  const modalId = buttonClicked.dataset.modalId;
-
-  const modalToOpen = document.getElementById('modalId');
-  modalToOpen.classList.toggle('hidden');
-  overlay.classList.toggle('hidden');
+const openAndCloseModal = function (modalId) {
+  const modalToOpen = document.getElementById(modalId);
+  if (modalToOpen) {
+    modalToOpen.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+  } else {
+    console.error("Couldn't find a modal element with ID:", modalId);
+  }
 };
 
 openModalBtn.forEach(function (btn) {
-  btn.addEventListener('click', openAndCloseModal);
+  btn.addEventListener('click', function (event) {
+    const modalId = event.target.dataset.modalId;
+    openAndCloseModal(modalId);
+  });
 });
 
-if (closeModalBtn) {
-  closeModalBtn.addEventListener('click', function () {
-    const openModals = document.querySelectorAll('.modal:not(.hidden)');
-    openModals.forEach(modal => modal.classList.add('hidden'));
+closeModalBtn.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    const modalId = btn.closest('.modal').id;
+    openAndCloseModal(modalId);
   });
-}
+});
+
+overlay.addEventListener('click', function () {
+  const openModals = document.querySelectorAll('.modal:not(.hidden)');
+  if (openModals.length > 0) {
+    openModals.forEach(function (modal) {
+      modal.classList.add('hidden');
+    });
+    overlay.classList.add('hidden');
+  }
+});
+
+// Handling KeyboardEvent
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'enter') {
+    if (!modal.classList.contains('hidden')) {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden');
+    }
+  }
+});
